8124795adca1ac131faae3e73e30de2dec651072
smile <you@example.com>
2024-10-16 12:44:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1></h1>
+     <div className='text-5x1 text-blue-400'>smile</div>
+    </div>
+  )
+}
+
+export default App
